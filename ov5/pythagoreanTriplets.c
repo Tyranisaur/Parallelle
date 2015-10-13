6eeeecaa26cecbd21d5fa0cf58498433c66e4d72
torgeiws <torgeiws@stud.ntnu.no>
2015-10-13 21:20:28 +0200

    implement mpi

--- ov5/pythagoreanTriplets.c
+++ ov5/pythagoreanTriplets.c
@@ -27,46 +27,59 @@ gcd ( int a, int b )
 int main(int argc, char **argv) {
 	char *inputLine = NULL; size_t lineLength = 0;
 	int *start, *stop, *numThreads, amountOfRuns = 0;
+	int myRank = 0, totalRanks = 1;
+
 
 #ifdef HAVE_MPI
-	MPI_Init(NULL, NULL);
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+	MPI_Comm_size(MPI_COMM_WORLD, &totalRanks);
 #endif
 
-	// Read in first line of input
-	getline(&inputLine, &lineLength, stdin);
-	sscanf(inputLine, "%d", &amountOfRuns);
+	if(myRank == 0)
+	{
+		// Read in first line of input
+		getline(&inputLine, &lineLength, stdin);
+		sscanf(inputLine, "%d", &amountOfRuns);
 
-	stop = (int*) calloc(amountOfRuns, sizeof(int));
-	start = (int*) calloc(amountOfRuns, sizeof(int));
-	numThreads = (int*) calloc(amountOfRuns, sizeof(int));
+		stop = (int*) calloc(amountOfRuns, sizeof(int));
+		start = (int*) calloc(amountOfRuns, sizeof(int));
+		numThreads = (int*) calloc(amountOfRuns, sizeof(int));
 
-	int tot_threads, current_start, current_stop;
-	for (int i = 0; i < amountOfRuns; ++i){
+		int tot_threads, current_start, current_stop;
+		for (int i = 0; i < amountOfRuns; ++i){
 
-		// Read in each line of input that follows after first line
-		free(inputLine); lineLength = 0; inputLine = NULL;
-		ssize_t readChars = getline(&inputLine, &lineLength, stdin);
+			// Read in each line of input that follows after first line
+			free(inputLine); lineLength = 0; inputLine = NULL;
+			ssize_t readChars = getline(&inputLine, &lineLength, stdin);
 
-		// If there exists at least two matches (2x %d)...
-		int matches =sscanf(inputLine, "%d %d %d", &current_start, &current_stop, &tot_threads);
-		if (matches >= 2){
-			if(current_start < 0 || current_stop < 0 || current_stop <= current_start){
-				current_start = 0, current_stop = 0;
+			// If there exists at least two matches (2x %d)...
+			int matches =sscanf(inputLine, "%d %d %d", &current_start, &current_stop, &tot_threads);
+			if (matches >= 2){
+				if(current_start < 0 || current_stop < 0 || current_stop <= current_start){
+					current_start = 0, current_stop = 0;
+				}
+				stop[i] = current_stop;
+				start[i] = current_start;
+				numThreads[i] = 1;
 			}
-			stop[i] = current_stop;
-			start[i] = current_start;
-			numThreads[i] = 1;
-		}
-		if(matches >= 3)
-		{
-			numThreads[i] = tot_threads;
-		}
-		if(matches < 2)
-		{
-			stop[i] = 0;
-			start[i] = 0;
+			if(matches >= 3)
+			{
+				numThreads[i] = tot_threads;
+			}
+			if(matches < 2)
+			{
+				stop[i] = 0;
+				start[i] = 0;
+			}
 		}
 	}
+#ifdef HAVE_MPI
+	MPI_Bcast (&amountOfRuns, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast (start, amountOfRuns, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast (stop, amountOfRuns, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast (numThreads, amountOfRuns, MPI_INT,    0, MPI_COMM_WORLD);
+#endif
 
 	/*
 	 *	Remember to only print 1 (one) sum per start/stop.
@@ -74,16 +87,21 @@ int main(int argc, char **argv) {
 	 */
 	int globalSum;
 	int c;
+
+
 	for(int i = 0; i < amountOfRuns; i++)
 	{
 		globalSum = 0;
-		if(stop[i] == 0)
+		if(myRank == 0)
 		{
-			printf("%d\n", globalSum);
-			continue;
+			if(stop[i] == 0)
+			{
+				printf("%d\n", globalSum);
+				continue;
+			}
 		}
 
-		for(int m = 2; m < stop[i]; m++)
+		for(int m = 2 + myRank; m < stop[i]; m += totalRanks)
 		{
 			int localSum = 0;
 
@@ -104,11 +122,14 @@ int main(int argc, char **argv) {
 			}
 			globalSum += localSum;
 		}
+#ifdef HAVE_MPI
+		MPI_Reduce(myRank ? &globalSum : MPI_IN_PLACE, &globalSum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+#endif
 
-
-
-
-		printf("%d\n", globalSum);
+		if(myRank == 0)
+		{
+			printf("%d\n", globalSum);
+		}
 	}
 	return 0;
 }
