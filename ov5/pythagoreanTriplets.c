e9b30b13a7dce124c8b0684208b1c160935f54e7
torgeiws <torgeiws@stud.ntnu.no>
2015-10-13 21:59:51 +0200

    fix omp pragma

--- ov5/pythagoreanTriplets.c
+++ ov5/pythagoreanTriplets.c
@@ -110,7 +110,7 @@ int main(int argc, char **argv) {
 			//localSum is the sum across OMP threads
 			int localSum = 0;
 
-#pragma 	omp parallel for shared(localSum) num_threads(numThreads[i]) reduction( +: localSum)
+#pragma 	omp parallel for num_threads(numThreads[i]) reduction( +: localSum)
 			for(int n = 1; n < m; n++)
 			{
 				//innerSum is the sum within each loop iteration done by a thread
